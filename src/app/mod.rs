5a9d574df6eaa188af294eb992f31c1d6dc653c8
Clomance <Hisoskar@gmail.com>
2021-06-20 21:54:12 +0300

    Returned removed files.

---
+++ src/app/mod.rs
@@ -0,0 +1,20 @@
+#[cfg(target_os="windows")]
+mod windows;
+#[cfg(target_os="windows")]
+pub use windows::{
+    App,
+    AppAttributes,
+    AppControl,
+    CursorIcon,
+    Background,
+    Fullscreen,
+    WindowAttributes,
+    WindowClassAttributes,
+    Event,
+    WindowEvent,
+    VirtualKeyCode,
+    EventLoopAttributes,
+    OpenGLRenderContextAttributes,
+    UpdateInterval,
+    WinError,
+};
